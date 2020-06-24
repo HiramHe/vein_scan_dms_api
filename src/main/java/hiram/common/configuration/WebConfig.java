0806f77fd65f4511a27a06aa055712456ca11c68
HiramHe <776748935@qq.com>
2020-06-24 23:13:35 +0800

    config swagger static resource handler.

--- src/main/java/hiram/common/configuration/WebConfig.java
+++ src/main/java/hiram/common/configuration/WebConfig.java
@@ -21,7 +21,7 @@ public class WebConfig extends WebMvcConfigurationSupport {
      * 不要在yml配置文件中配置spring.mvc.static-path-pattern,
      * 否则所有的静态资源都需要走该配置的值才能访问到。
      * 而我希望的的是：swagger走：localhost:9090/swagger-ui.html，
-     * 图片资源走：localhost:9090/static/xxx.png。
+     * 图片资源走：localhost:9090/picture/xxx.png。
      * @param registry
      */
     @Override
@@ -30,6 +30,16 @@ public class WebConfig extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/picture/**")
                 .addResourceLocations("file:" + pictureProperties.getInfraredDirectory())
                 .addResourceLocations("file:" + pictureProperties.getUltrasoundDirectory())
+        ;
+
+        /*
+        swagger静态资源映射
+         */
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/")
+        ;
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/")
         ;
 
         super.addResourceHandlers(registry);
